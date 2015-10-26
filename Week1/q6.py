9183bc8b2691131d80b75e24ff2da0c5efbcf652
jrgreenberg <jon.r.greenberg@gmail.com>
2015-10-26 12:23:45 +0000

    updated Question 6 and 7

--- Week1/q6.py
+++ Week1/q6.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python
 import yaml
-
+import json
+# create list
 list1 = range(10)
 list1.append('hello world')
 list1.append( 'this is fun')
 list1.append({'IP Address': '5.5.5.5'})
 list1.append({'numbers': range(20)})
+
+# export list to YAML
 with open("q6yaml.yml", "w") as f:
     f.write("-" * 3)
     f.write("\n")
     f.write(yaml.dump(list1,default_flow_style=False))
+# export list to JSON
 
+with open("q6json.json","w") as f:
+    json.dump(list1,f)
 
-print type(list1)
-print len(list1)
-print list1
+#print type(list1)
+#print len(list1)
+#print list1
--- Week1/q7.py
+++ Week1/q7.py
@@ -1,5 +1,18 @@
 import yaml
+import json
 from pprint import pprint
 with open ("q6yaml.yml") as f:
     yaml_data = yaml.load(f)
+with open ("q6json.json") as ff:
+    json_data = json.load(ff)
+
+seperator = "-" * 20
+print seperator
+pprint("YAML output")
+print seperator
 pprint(yaml_data)
+print seperator
+pprint("JSON output")
+print seperator
+pprint(json_data)
+print seperator
